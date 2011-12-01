3e030783f02db4a5447cddb8c2a1da495751f2f0
Mark Glenn <markglenn@gmail.com>
2011-11-30 20:57:34 -0600

    Initial commit

---
+++ lib/guard-nunit.rb
@@ -0,0 +1,7 @@
+require "guard-nunit/version"
+
+module Guard
+  module Nunit
+    # Your code goes here...
+  end
+end
---
+++ lib/guard-nunit/version.rb
@@ -0,0 +1,5 @@
+module Guard
+  module Nunit
+    VERSION = "0.0.1"
+  end
+end
